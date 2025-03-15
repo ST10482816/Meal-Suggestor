6b264ebddf2b6ed58e3f8aff739e81521738cc69
ST10482816 <ST10482816@vcconnect.edu.za>
2025-03-15 16:48:08 +0200

    submit button logic updated including error handling. Adjusted generateMeal and resetInputFields functions

--- app/src/main/java/za/co/varsitycollege/st10482816/mealsuggestor/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10482816/mealsuggestor/MainActivity.kt
@@ -1,8 +1,11 @@
 package za.co.varsitycollege.st10482816.mealsuggestor
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,7 +23,37 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
+        var txtSuggestionResult = findViewById<TextView>(R.id.txtSuggestionResult)
+        var timeOfDay = findViewById<EditText>(R.id.txeTimeOfDay)
 
+        btnSubmit.setOnClickListener{
+
+//            var timeOfDayText = timeOfDay.text.toString()
+
+//          checks if user input is not blank nor whitespaces
+            if (timeOfDay.text.isNotBlank()) {
+                try {
+//              tries to stringify user input and store in timeOfDayText variable
+                    var timeOfDayText = timeOfDay.text.toString()
+
+                    txtSuggestionResult.text = timeOfDayText
+
+                } catch (e: Exception){
+//                  Logs the error in console
+                    Log.e("btnSubmit", "Error:  ${e.message}")
+
+//                  Displays error message to user
+                    Toast.makeText(this, "Make sure you chose an appropriate time of day", Toast.LENGTH_SHORT).show()
+//                  resetInputFields(timeOfDay, txtSuggestionResult)
+                }
+            }
+
+
+
+//
+
+        }
 
 
 
@@ -34,7 +67,8 @@ class MainActivity : AppCompatActivity() {
     }
 
 //    function resets fields when called
-    private fun resetInputFields(){
-
+    private fun resetInputFields(timeOfDay: String, txtSuggestionResult: String){
+//        timeOfDay.text = ""
+//        txtSuggestionResult.text = ""
     }
 }
