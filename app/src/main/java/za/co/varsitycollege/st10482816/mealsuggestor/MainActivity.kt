9e779bdbb55319f02d1cc9cce81eeb1b7d28a941
ST10482816 <ST10482816@vcconnect.edu.za>
2025-03-15 16:03:43 +0200

    TextViews, EditTexts and Buttons added

--- app/src/main/java/za/co/varsitycollege/st10482816/mealsuggestor/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10482816/mealsuggestor/MainActivity.kt
@@ -1,6 +1,8 @@
 package za.co.varsitycollege.st10482816.mealsuggestor
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
+
+
+
+    }
+
+
+
+//    function generates a meal based on the user's chosen input
+    private fun generateMeal(){
+
+    }
+
+//    function resets fields when called
+    private fun resetInputFields(){
+
     }
 }
